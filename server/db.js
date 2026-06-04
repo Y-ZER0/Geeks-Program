521488ce2b02fd0ba3d173ba24990f63610559da
Yousef Abu-nimreh <yousedosama133@gmail.com>
2026-06-04 23:20:44 +0300

    test

--- server/db.js
+++ server/db.js
@@ -4,7 +4,9 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  console.error("Missing SUPABASE_URL or SUPABASE_SERVICE_KEY in environment");
+  throw new Error(
+    "Missing required environment variables: SUPABASE_URL and/or SUPABASE_SERVICE_KEY"
+  );
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
