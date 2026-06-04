317a2c82e5a966503f71a5becf015698d00c6c75
Yousef Abu-nimreh <yousedosama133@gmail.com>
2026-06-04 21:09:12 +0300

    dsa

--- server/db.js
+++ server/db.js
@@ -5,7 +5,6 @@ const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.error("Missing SUPABASE_URL or SUPABASE_SERVICE_KEY in environment");
-  process.exit(1);
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
